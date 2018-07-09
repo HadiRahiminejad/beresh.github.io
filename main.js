e818e1be4b98222880ac5ab43a1ec60e8d9ef2b5
beresh <33800915+beresh@users.noreply.github.com>
2018-07-09 20:30:21 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+   var mySrc = myImage.getAttribute('src');
+   if(mySrc ==='images/index.jpg' ){
+     myImage.setAttribute('src','images/images.jpg');
+   }
+   else {
+     myImage.setAttribute('src','images/index.jpg');
+   }
+
+}
+
+/*var mybtn = document.querySelector('button');
+var myheading =document.querySelector('h1');
+
+function setUserName() {
+   var myName = prompt('Please enter your name.');
+   localStorage.setItem('name' , myName);/*an API called localStorage,
+   which allows us to store data in the browser and later retrieve it*/
+  /* myheading.textContent = 'Mozilla is cool ' + myName;
+}
+
+if(!localStorage.getItem('name')){
+  setUserName();
+}else {
+  var storedName = localStorage.getItem('name');
+  myheading.textContent = 'Mozilla is cool ' + storedName;
+}
+
+mybtn.onclick = function() {
+  setUserName();
+}*/
+
+var mybtn =document.querySelector('button');
+var myheading = document.querySelector('h1');
+
+function setUserName() {
+
+  var myName = prompt('Please enter your name');
+  localStorage.setItem('name' ,myName);
+  myheading,textContent = 'Mozzila is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')){
+  setUserName();
+}else {
+  var storedName = localStorage.getItem('name');
+  myheading.textContent = 'mozilla is cool, ' + storedName;
+}
+
+mybtn.onclick = function(){
+  setUserName();
+}
