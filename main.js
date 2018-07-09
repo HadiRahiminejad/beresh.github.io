dd964ed3d3e14669dfffbeb2d380c45e629d926a
beresh <33800915+beresh@users.noreply.github.com>
2018-07-09 20:34:07 +0430

    Update main.js

--- main.js
+++ main.js
@@ -2,35 +2,14 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
    var mySrc = myImage.getAttribute('src');
-   if(mySrc ==='images/index.jpg' ){
-     myImage.setAttribute('src','images/images.jpg');
+   if(mySrc ==='index.jpg' ){
+     myImage.setAttribute('src','images.jpg');
    }
    else {
-     myImage.setAttribute('src','images/index.jpg');
+     myImage.setAttribute('src','index.jpg');
    }
 
 }
-
-/*var mybtn = document.querySelector('button');
-var myheading =document.querySelector('h1');
-
-function setUserName() {
-   var myName = prompt('Please enter your name.');
-   localStorage.setItem('name' , myName);/*an API called localStorage,
-   which allows us to store data in the browser and later retrieve it*/
-  /* myheading.textContent = 'Mozilla is cool ' + myName;
-}
-
-if(!localStorage.getItem('name')){
-  setUserName();
-}else {
-  var storedName = localStorage.getItem('name');
-  myheading.textContent = 'Mozilla is cool ' + storedName;
-}
-
-mybtn.onclick = function() {
-  setUserName();
-}*/
 
 var mybtn =document.querySelector('button');
 var myheading = document.querySelector('h1');
