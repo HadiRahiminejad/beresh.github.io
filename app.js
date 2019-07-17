7687084729b0e411454cafdd060827feb299aa97
beresh <33800915+beresh@users.noreply.github.com>
2019-07-17 07:49:46 +0430

    Update app.js

--- app.js
+++ app.js
@@ -13,36 +13,28 @@ function newGame() {
    document.getElementById('score-1').textContent = '0';
    document.getElementById('current-0').textContent = '0';
    document.getElementById('current-1').textContent = '0';
+   document.getElementById('name-0').textContent = 'Player 1';
+   document.getElementById('name-1').textContent = 'Player 2';
+   document.querySelector('.player-0-panel').classList.add('active');
    
-   var score0 = document.getElementById('score-0');
-   var score1 = document.getElementById('score-1');
-   var current0 = document.getElementById('current-0');
-   var current1 = document.getElementById('current-1');
-   
-  
     document.querySelector('.btn-roll').addEventListener('click', function() {
        
        // 1. random number
        var dice = Math.floor(Math.random() * 6) + 1;
      
-       
-       // 2.display number
-        
+       // 2.display number     
        var diceDom = document.querySelector('.dice');
        diceDom.style.display = 'block';
         diceDom.src = 'dice-' + dice + '.png';
    
         // 3. Update the round score If the rolled number was NOT a 1
-   
            if(dice !== 1) {
               roundScore += dice;
               document.querySelector('#current-' + activePlayer).textContent = roundScore;
            } else{
            
             changPlayer();
-   
-           }
-             
+           }    
    });
    
    document.querySelector('.btn-hold').addEventListener('click', function() {
@@ -65,9 +57,6 @@ function newGame() {
       }else{
          changPlayer();
       }
-   
-     
-   
    });
    
    function changPlayer() {
@@ -89,6 +78,6 @@ newGame();
 
 document.querySelector('.btn-new').addEventListener('click' , function(){
    newGame();
-})
+});
 
 
